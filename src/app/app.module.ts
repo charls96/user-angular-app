189b9b83512bce9814a737ee5e9f387316ee23cc
Carlos Daniel Lopez Perez <cloppere@emeal.nttdata.com>
2022-03-29 10:42:19 +0200

    shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 
-//Angular material
-import { MatSliderModule } from '@angular/material/slider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+//Shared module
+import { SharedModule } from './components/shared/shared.module';
 
 //Components
 import { LoginComponent } from './components/login/login.component';
@@ -26,13 +20,7 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
