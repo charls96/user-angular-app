d6811176b64548ce43cef69044665b7be6a0da7e
Carlos Daniel Lopez Perez <cloppere@emeal.nttdata.com>
2022-03-29 12:29:06 +0200

    users filter, sorting and pagination

--- src/app/components/dashboard/users/users.component.ts
+++ src/app/components/dashboard/users/users.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from 'src/app/interfaces/user';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+
 
 @Component({
   selector: 'app-users',
@@ -7,9 +19,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  listUsers: User[] = [
+    {user: 'carlos', name: 'Carlos', surname: 'Daniel', gender: 'male'},
+    {user: 'juanB', name: 'Juan', surname: 'malo', gender: 'male'},
+    {user: 'vaness', name: 'Vanesa', surname: 'Villa', gender: 'female'},
+    {user: 'geo', name: 'Geovanny', surname: 'Podesta', gender: 'male'},
+    {user: 'almu', name: 'almudena', surname: 'Vivancos', gender: 'female'},
+    {user: 'fede', name: 'Federico', surname: 'Pastor', gender: 'male'},
+    {user: 'serito', name: 'Sergio', surname: 'Buendia', gender: 'male'},
+  ];
+
+  displayedColumns: string[] = ['user', 'name', 'surname', 'gender', 'actions'];
+  dataSource = new MatTableDataSource(this.listUsers);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -14,6 +14,10 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 
 @NgModule({
@@ -30,6 +34,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     MatIconModule,
     HttpClientModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   exports: [
     MatSliderModule,
@@ -42,6 +50,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     MatIconModule,
     HttpClientModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule,
   ]
 })
 export class SharedModule { }
